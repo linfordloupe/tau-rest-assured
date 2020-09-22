8d3604dd3eda47b21bbf8cacccd12b08570d4ae8
linford Copeland <linford@loupetestware.com>
2020-09-22 21:09:20 +0100

    playing around

--- src/test/java/examples/Chapter1Test.java
+++ src/test/java/examples/Chapter1Test.java
@@ -1,5 +1,6 @@
 package examples;
 
+import org.checkerframework.common.value.qual.StaticallyExecutable;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.*;
@@ -9,12 +10,21 @@ public class Chapter1Test {
 
     @Test
     public void requestUsZipCode90210_checkPlaceNameInResponseBody_expectBeverlyHills() {
-
-        given().
+        given().log().all().
         when().
             get("http://zippopotam.us/us/90210").
+
         then().
             assertThat().
-            body("places[0].'place name'", equalTo("Beverly Hills"));
+            body("places[0].'place name'", equalTo("Beverly Hills")).
+                    log().body();
     }
+
+
+    @Test
+    public void getResponseBody() {
+        get("http://zippopotam.us/us/90210").then().log().body();
+
+    }
+
 }
